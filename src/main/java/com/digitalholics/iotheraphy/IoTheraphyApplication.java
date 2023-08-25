3cc153fce77022a9f07ae1cf0e46120d1e17c497
Api System IoTheraphy <u202020239@upc.edu.pe>
2023-08-25 13:58:03 -0500

    Initial commit

---
+++ src/main/java/com/digitalholics/iotheraphy/IoTheraphyApplication.java
@@ -0,0 +1,13 @@
+package com.digitalholics.iotheraphy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IoTheraphyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IoTheraphyApplication.class, args);
+	}
+
+}
