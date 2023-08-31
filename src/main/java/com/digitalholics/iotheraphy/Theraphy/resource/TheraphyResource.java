52414c4a3ead351176d6e6ce0450cf76469fcdfa
leona <u202018777@upc.edu.pe>
2023-08-30 23:16:38 -0500

    feat(theraphy): Added model, service and controller of theraphy.

---
+++ src/main/java/com/digitalholics/iotheraphy/Theraphy/resource/TheraphyResource.java
@@ -0,0 +1,35 @@
+package com.digitalholics.iotheraphy.Theraphy.resource;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+import lombok.*;
+
+import java.util.Date;
+
+@Getter
+@Setter
+@With
+@NoArgsConstructor
+@AllArgsConstructor
+public class TheraphyResource {
+
+
+    private Integer id;
+
+
+    private String theraphyName;
+
+    private String appointmentQuantity;
+
+
+    private String appointmentGap;
+
+    private Date startAt;
+
+    private Date finishAt;
+}
