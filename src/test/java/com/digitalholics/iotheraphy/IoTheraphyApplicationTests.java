4d4a7e7cf92050b0d6df14b36ae5df537b3cbb6b
JaredD30 <u202020239@upc.edu.p>
2023-11-22 22:25:22 -0500

    feat: Added Test Class

---
+++ src/test/java/com/digitalholics/iotheraphy/IoTheraphyApplicationTests.java
@@ -0,0 +1,12 @@
+package com.digitalholics.iotheraphy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class IoTheraphyApplicationTests {
+    @Test
+    void contextLoads() {
+
+    }
+}
